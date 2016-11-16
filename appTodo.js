603e45d18185bf5981dc54be26ecfbf45b21aef9
poposs <57660009@go.buu.ac.th>
2016-11-16 13:19:04 +0700

    create controller and service for add title on table

---
+++ appTodo.js
@@ -0,0 +1,31 @@
+angular.module('TodoApp', [])
+.service('contactService', function(){
+  var self = this
+
+  self.title_index = [{title: 'text1', done: true},
+                   {title: 'text2', done: false} ]
+
+  self.list = function(){
+    return self.title_index;
+  }
+
+  self.add = function(contact){
+    self.title_index.push(contact);
+  }
+})
+
+.controller('ListTitleController', function($scope, TitleService){
+    $scope.title_index = TitleService.list();
+})
+
+.controller('AddTitleController', function($scope, TitleService){
+
+    $scope.save = function () {
+      var titles = {title: $scope.title, done: false }
+      contactService.add(titles);
+      resetForm();
+    }
+    function resetForm() {
+      $scope.title = '';
+    }
+});
